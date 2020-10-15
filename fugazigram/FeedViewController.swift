f275831bf50a6484d6c902cfad4416ced61c2895
Cyrus Simons <cyrus.simons@yahoo.com>
2020-10-14 21:46:50 -0400

    completed lab

--- fugazigram/FeedViewController.swift
+++ fugazigram/FeedViewController.swift
@@ -6,13 +6,78 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [PFObject]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
+
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className:"Posts")
+        query.includeKey("owner")
+        query.limit = 20
+        
+        /*query.findObjectsInBackground { (postsarr, error) in
+            if postsarr != nil {
+                self.posts = postsarr!
+                self.tableView.reloadData()
+            } else{ print("error loading posts. None to load.") }
+        }*/
+        
+        query.findObjectsInBackground { (objects: [PFObject]?, error: Error?) in
+            if let error = error {
+                // Log details of the failure
+                print(error.localizedDescription)
+            } else if let objects = objects {
+                // The find succeeded.
+                print("Successfully retrieved \(objects.count) scores.")
+                // Do something with the found objects
+                for object in objects {
+                    print(object.objectId as Any)
+                }
+                self.posts = objects
+                self.tableView.reloadData()
+            }
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        
+        let post = posts[indexPath.row]
+        
+        let user = post["owner"] as! PFUser
+        cell.unameLabel.text = user.username
+        cell.captionLabel.text = post["caption"] as? String
+        
+        let imageFile = post["image"] as! PFFileObject
+        
+        let urlstring = imageFile.url!
+        let url = URL(string: urlstring)!
+        
+        cell.photoView.af.setImage(withURL: url)
+        
+        return cell
     }
     
 
