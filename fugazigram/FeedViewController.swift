3251eb93eb99e604d2c4245cc8695519c7aea13c
Cyrus Simons <cyrus.simons@yahoo.com>
2020-10-16 12:17:15 -0400

    refresh, comments done

--- fugazigram/FeedViewController.swift
+++ fugazigram/FeedViewController.swift
@@ -15,12 +15,17 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     var posts = [PFObject]()
     
+    let postRefreshControl = UIRefreshControl()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.delegate = self
         tableView.dataSource = self
+        
+        postRefreshControl.addTarget(self, action: #selector(viewDidAppear(_:)), for: .valueChanged)
+        tableView.refreshControl = postRefreshControl
 
         // Do any additional setup after loading the view.
     }
@@ -29,7 +34,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         super.viewDidAppear(animated)
         
         let query = PFQuery(className:"Posts")
-        query.includeKey("owner")
+        query.includeKeys(["owner", "comments", "comments.owner"])
         query.limit = 20
         
         /*query.findObjectsInBackground { (postsarr, error) in
@@ -52,34 +57,77 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
                 }
                 self.posts = objects
                 self.tableView.reloadData()
+                self.postRefreshControl.endRefreshing()
             }
         }
         
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.posts.count
+        let post = posts[section]
+        
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        
+        return comments.count + 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        let post = posts[indexPath.section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
         
-        let post = posts[indexPath.row]
+        if indexPath.row == 0 {
         
-        let user = post["owner"] as! PFUser
-        cell.unameLabel.text = user.username
-        cell.captionLabel.text = post["caption"] as? String
-        
-        let imageFile = post["image"] as! PFFileObject
-        
-        let urlstring = imageFile.url!
-        let url = URL(string: urlstring)!
-        
-        cell.photoView.af.setImage(withURL: url)
-        
-        return cell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+            
+            
+            let user = post["owner"] as! PFUser
+            cell.unameLabel.text = user.username
+            cell.captionLabel.text = post["caption"] as? String
+            
+            let imageFile = post["image"] as! PFFileObject
+            
+            let urlstring = imageFile.url!
+            let url = URL(string: urlstring)!
+            
+            cell.photoView.af.setImage(withURL: url)
+            
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell") as! CommentCell
+            
+            let comment = comments[indexPath.row - 1]
+            
+            cell.commentLabel.text = comment["text"] as? String
+            
+            let user = comment["owner"] as! PFUser
+            cell.uNameLabel.text = user.username
+            
+            return cell
+        }
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let post = posts[indexPath.row]
+        
+        let comment = PFObject(className: "Comments")
+        comment["text"] = "random text"
+        comment["post"] = post
+        comment["owner"] = PFUser.current()!
+        
+        post.add(comment, forKey: "comments")
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print("comment saved")
+            }else{ print("error saving comment") }
+        }
+        
+    }
     
     @IBAction func onLogout(_ sender: Any) {
         PFUser.logOut()
